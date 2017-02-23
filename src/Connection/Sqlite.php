d674b74eea358fdce29d6c5c75bf81eaa0117948
Christoph Kappestein <k42b3.x@gmail.com>
2017-02-23 12:26:20 +0100

    add sqlite connection

---
+++ src/Connection/Sqlite.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Fusio\Adapter\Sample\Connection;
+
+use Doctrine\DBAL\DriverManager;
+use Fusio\Engine\ConnectionInterface;
+use Fusio\Engine\Form\BuilderInterface;
+use Fusio\Engine\Form\ElementFactoryInterface;
+use Fusio\Engine\ParametersInterface;
+
+class Sqlite implements ConnectionInterface
+{
+    public function getName()
+    {
+        return 'Sqlite';
+    }
+
+    public function getConnection(ParametersInterface $config)
+    {
+        return DriverManager::getConnection([
+            'path'   => PSX_PATH_CACHE . '/todo-app.db',
+            'driver' => 'pdo_sqlite',
+        ]);
+    }
+
+    public function configure(BuilderInterface $builder, ElementFactoryInterface $elementFactory)
+    {
+    }
+}
